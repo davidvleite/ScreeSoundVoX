8ce7b11169cd355aac0414668de58d92e37f5757
David Veríssimo Leite <103391404+davidvleite@users.noreply.github.com>
2024-11-13 18:53:46 -0300

    Implementação da lógica para login e logout

--- ScreenSound.Web/Program.cs
+++ ScreenSound.Web/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -7,11 +8,17 @@ using ScreenSound.Web.Services;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
 builder.Services.AddMudServices();
+
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthAPI>();
+builder.Services.AddScoped<AuthAPI>(sp => (AuthAPI)sp.GetRequiredService<AuthenticationStateProvider>());
+
 builder.Services.AddScoped<CookieHandler>();
 builder.Services.AddScoped<ArtistaAPI>();
 builder.Services.AddScoped<MusicasAPI>();
-builder.Services.AddScoped<AuthAPI>();
+
 builder.Services.AddHttpClient("API", client => {
     client.BaseAddress = new Uri(builder.Configuration["APIServer:Url"]!);
     client.DefaultRequestHeaders.Add("Accept", "application/json");
---
+++ ScreenSound.Web/Response/InforPessoaResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace ScreenSound.Web.Response
+{
+    public class InforPessoaResponse
+    {
+        public string? Email { get; set; }
+        public bool IsEmailConfirmed { get; set; }
+    }
+}
